579d45e738c5ff717324b396f065de330fde375f
denispeplin <denis.peplin@gmail.com>
2016-07-03 22:00:58 +0300

    Search for Gitlab CI message in a first attachment.

--- lib/mentioner/slack.ex
+++ lib/mentioner/slack.ex
@@ -1,6 +1,20 @@
 defmodule Mentioner.Slack do
   use Slack
 
+  def handle_message(message = %{attachments: [attachment | _tail]}, slack, state) do
+    IO.puts inspect(attachment)
+    cond do
+      parser_data = Regex.run(~r/Commit.*branch by ([^ ]+) ([^ ]+) /, attachment.fallback) ->
+        [_, username, result] = parser_data
+        user = Mentioner.Usermap.get username
+        if result == "failed" && user do
+          send_message("<@#{user}> :point_up_2:", message.channel, slack)
+        end
+      true ->
+        true
+    end
+    { :ok, state }
+  end
   def handle_message(message = %{type: "message"}, slack, state) do
     IO.puts inspect(message)
     cond do
@@ -10,12 +24,6 @@ defmodule Mentioner.Slack do
         [_, github_username] = parser_data
         Mentioner.Usermap.put(github_username, message.user)
         send_message("<@#{message.user}> subscribed to #{github_username}", message.channel, slack)
-      parser_data = Regex.run(~r/Commit.*branch by ([^ ]+) ([^ ]+) /, message.text) ->
-        [_, username, result] = parser_data
-        user = Mentioner.Usermap.get username
-        if result == "failed" && user do
-          send_message("<@#{user}> :point_up_2:", message.channel, slack)
-        end
       true ->
         true
     end
