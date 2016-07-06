4c98e12662604b6c54be54086368e8eae5c5e0e4
denispeplin <denis.peplin@gmail.com>
2016-07-06 19:53:33 +0300

    Refactor code, handle commands by separate functions.

--- lib/mentioner/slack.ex
+++ lib/mentioner/slack.ex
@@ -1,36 +1,39 @@
 defmodule Mentioner.Slack do
   use Slack
 
+  @doc """
+  Handling messages from Slack chat.
+  """
   def handle_message(message = %{attachments: [attachment | _tail]}, slack, state) do
     IO.puts inspect(attachment)
-    cond do
-      parser_data = Regex.run(~r/Commit.*branch by (.+) (passed|failed) /, attachment.fallback) ->
-        [_, username, result] = parser_data
-        user = Mentioner.Usermap.get username
-        if result == "failed" && user do
-          send_message("<@#{user}> :point_up_2:", message.channel, slack)
-        end
-      true ->
-        true
+    with [_, username, result] <- Regex.run(~r/Commit.*branch by (.+) (passed|failed) /, attachment.fallback) do
+      user = Mentioner.Usermap.get username
+      if result == "failed" && user do
+        send_message("<@#{user}> :point_up_2:", message.channel, slack)
+      end
     end
     { :ok, state }
   end
   def handle_message(message = %{type: "message"}, slack, state) do
     IO.puts inspect(message)
-    cond do
-      Regex.run ~r/<@#{slack.me.id}>:?\sping/, message.text ->
-        send_message("<@#{message.user}> pong", message.channel, slack)
-      parser_data = Regex.run(~r/<@#{slack.me.id}>:?\ssubscribe[\s]+(.+)/, message.text) ->
-        [_, github_username] = parser_data
-        github_username |> String.strip |> Mentioner.Usermap.put(message.user)
-        send_message("<@#{message.user}> subscribed to #{github_username}", message.channel, slack)
-      true ->
-        true
+    with [_, command, params] <- Regex.run ~r/<@#{slack.me.id}>:?\s([^ ]+)\s?(.*)/, message.text do
+      handle_command(command, params, message, slack)
     end
     {:ok, state}
   end
   # Catch all message handler so we don't crash
   def handle_message(_message, _slack, state) do
     {:ok, state}
+  end
+
+  @doc """
+  Handle direct commands.
+  """
+  def handle_command("ping", _, message, slack) do
+    send_message("<@#{message.user}> pong", message.channel, slack)
+  end
+  def handle_command("subscribe", gitlab_username, message, slack) do
+    gitlab_username |> String.strip |> Mentioner.Usermap.put(message.user)
+    send_message("<@#{message.user}> subscribed to #{gitlab_username}", message.channel, slack)
   end
 end
